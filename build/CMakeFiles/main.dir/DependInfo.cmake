e178101594677eb57cf3a28abe4f65c7d4998c24
zengzhengqi0524 <912839134@qq.com>
2021-04-10 19:31:47 +0800

    first commit

---
+++ build/CMakeFiles/main.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/IRON/Desktop/tdoa_c/GCC-PHAT/DelaySum.c" "CMakeFiles/main.dir/DelaySum.c.obj" "gcc" "CMakeFiles/main.dir/DelaySum.c.obj.d"
+  "C:/Users/IRON/Desktop/tdoa_c/GCC-PHAT/kiss_fft.c" "CMakeFiles/main.dir/kiss_fft.c.obj" "gcc" "CMakeFiles/main.dir/kiss_fft.c.obj.d"
+  "C:/Users/IRON/Desktop/tdoa_c/GCC-PHAT/kiss_fftr.c" "CMakeFiles/main.dir/kiss_fftr.c.obj" "gcc" "CMakeFiles/main.dir/kiss_fftr.c.obj.d"
+  "C:/Users/IRON/Desktop/tdoa_c/GCC-PHAT/main.c" "CMakeFiles/main.dir/main.c.obj" "gcc" "CMakeFiles/main.dir/main.c.obj.d"
+  "C:/Users/IRON/Desktop/tdoa_c/GCC-PHAT/wave.c" "CMakeFiles/main.dir/wave.c.obj" "gcc" "CMakeFiles/main.dir/wave.c.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ main.c
@@ -0,0 +1,75 @@
+/*
+ * @Author: your name
+ * @Date: 2021-04-10 14:42:57
+ * @LastEditTime: 2021-04-10 18:36:07
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \GCC-PHAT\main.c
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include "wave.h"
+#include "DelaySum.h"
+#include "kiss_fftr.h"
+#include "kiss_fft.h"
+#include "kiss_fftr.h"
+int main()
+{
+    FILE *fp;
+    int16_t samples = 4096;
+    int32_t buffer[samples];
+
+    fp = fopen("C:\\Users\\IRON\\Desktop\\tdoa_c\\GCC-PHAT\\BIN\\Yaw270Pitch30.bin", "rb+");
+    if (!fp)
+    {
+        printf("can't open file");
+        exit(1);
+    }
+
+    fread(buffer, 4, samples, fp);
+    fclose(fp);
+    printf("read ok!");
+
+    int8_t N = 8;
+
+    int32_t data[N][samples];
+
+    for (uint16_t i = 0; i < N; i++)
+    {
+        for (uint16_t j = 0; j < samples; j++)
+        {
+            data[i][j] = buffer[i + 8 * j];
+        }
+    }
+
+    /*
+    uint16_t fs = 48000;
+
+    uint32_t DataLen =4096;
+
+	float *yout = (float *)malloc(DataLen * sizeof(float));
+
+	float E[360];
+    float r = 0.0457;
+	for (uint16_t i = 0; i < 360; i++)
+	{
+		DelaySumURA(data, yout, fs, DataLen, N_FFT, WinLen, 256, r, i);
+		for (uint16_t j = 0; j < DataLen; j++)
+		{
+			E[j] = E[j] + yout[j] * yout[j];
+		}
+	}
+
+    //DelaySumURA(data, yout,fs, DataLen,N_FFT, WinLen, 256, r, 30);
+    
+	
+    for (uint16_t i = 0; i < 360; i++)
+    {
+        printf ("%f\n",E[i]);
+    }
+    free(yout);
+*/
+    getchar();
+    return 0;
+}
